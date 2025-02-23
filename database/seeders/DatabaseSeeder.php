86a336f9c1c53ae8418704ff991b985f872bec42
zahidulNahid <zahidulnahid7464@gmail.com>
2025-02-23 09:36:58 +0600

    update dbseed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'zahidul.bdcalling@gmail.com',
+            'password' => bcrypt('admin1234')
         ]);
     }
 }
