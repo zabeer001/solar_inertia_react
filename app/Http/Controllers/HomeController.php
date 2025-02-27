36ef43d2f4e79d834331c228cdff942060ea4241
zabeer001 <binzabirtareq@gmail.com>
2025-02-27 11:34:36 +0600

    e

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
              $siteDetails->gallery_image_2_url = $siteDetails->gallery_image_2 ? asset('storage/' . $siteDetails->gallery_image_2) : null;
              $siteDetails->logo_url = $siteDetails->logo ? asset('storage/' . $siteDetails->logo) : null;
          }
-         $homePageContent = Content::all();
+         $homePageContents = Content::all();
      
          return Inertia::render('Frontend/Home', compact('siteDetails','campaignDetails'));
      }
